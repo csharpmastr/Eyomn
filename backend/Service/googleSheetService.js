2bb0b7bf53faecd9ad4dea643bd9b188b13e6514
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-08-26 17:39:19 +0800

    Installed dependencies

--- backend/Service/googleSheetService.js
+++ backend/Service/googleSheetService.js
@@ -1,46 +1,40 @@
-const { google } = require('googleapis');
-require('dotenv').config()
+const { google } = require("googleapis");
+require("dotenv").config();
 
 const privateKey = process.env.GOOGLE_PRIVATE_KEY;
 if (!privateKey) {
-    throw new Error('GOOGLE_PRIVATE_KEY is not set in the environment variables');
+  throw new Error("GOOGLE_PRIVATE_KEY is not set in the environment variables");
 }
 
 const auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    },
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: privateKey.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
-const sheet = google.sheets({ version: 'v4', auth });
-const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+const sheet = google.sheets({ version: "v4", auth });
 
 async function appendToSheet(data) {
-    const values = [Object.values(data)];
-    const range = 'Sheet1';
-    const valueInputOption = 'USER_ENTERED';
-    const resource = { values };
+  const values = [Object.values(data)];
+  const range = "Sheet1";
+  const valueInputOption = "USER_ENTERED";
+  const resource = { values };
 
-    try {
-        const response = await sheet.spreadsheets.values.append({
-            spreadsheetId,
-            range,
-            valueInputOption,
-            resource,
-            insertDataOption: 'INSERT_ROWS'
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error writing to sheet:', error);
-        throw error;
-    }
+  try {
+    const response = await sheet.spreadsheets.values.append({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range,
+      valueInputOption,
+      resource,
+      insertDataOption: "INSERT_ROWS",
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error writing to sheet:", error);
+    throw error;
+  }
 }
 
 module.exports = { appendToSheet };
