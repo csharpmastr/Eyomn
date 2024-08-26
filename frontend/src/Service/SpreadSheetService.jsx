24e22f07162b01d8ea0d0721b650b54bdbafd9f5
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-08-26 16:45:12 +0800

    Changed endpoint

--- frontend/src/Service/SpreadSheetService.jsx
+++ frontend/src/Service/SpreadSheetService.jsx
@@ -1,18 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
-const AddUserToWaitlist = async(data) =>{
-    try{
-        const res = await axios.post('http://localhost:5000/api/sheet/submit', data,{
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        console.log(res);
-        return res.data
-    }catch(err){
-        console.log(err);
-        
-    }
-}
+const AddUserToWaitlist = async (data) => {
+  try {
+    const res = await axios.post(
+      "https://eyomn.vercel.app/api/sheet/submit",
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(res);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default AddUserToWaitlist;
