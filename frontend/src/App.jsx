1de371aaf203b68603da07783fd65f8a731ec88e
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-09-01 12:44:30 +0800

    Added Pixel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import AuthenticationPage from "./Page/AuthenticationPage";
 import Landing from "./Page/Landing";
-const App = () =>{
-    return (
-        <Router>
-            <Routes>
-            <Route path="/" element={<Landing/>} />
-            
-            <Route path="/login" element={<AuthenticationPage type="login" />} />
-            <Route path="/signup" element={<AuthenticationPage type="signup" /> } />
-            </Routes>
-        </Router>
-    )
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/*" element={<Landing />} />
+        {/* <Route path="/login" element={<AuthenticationPage type="login" />} />
+        <Route path="/signup" element={<AuthenticationPage type="signup" />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
