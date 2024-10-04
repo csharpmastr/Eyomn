41665b59675880dcab9fae41d70b7c2c1fab4e30
luisbandivas <jimboybandivas@gmail.com>
2024-10-04 09:05:01 +0800

    Added: Branch page, modal / Change: Organization page

---
+++ frontend/src/Component/ui/AddBranchModal.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AddBranchModal = ({ onClose }) => {
+  return ReactDOM.createPortal(
+    <div className="fixed top-0 left-0 flex items-center justify-center h-screen w-screen bg-black bg-opacity-30 z-50">
+      <div className="w-[500px]">
+        <header className="px-4 py-4 bg-bg-sb border border-b-f-gray rounded-t-lg flex justify-between">
+          <h1 className="text-p-lg text-c-secondary font-semibold">
+            Add Branch
+          </h1>
+          <button onClick={onClose}> &times; </button>
+        </header>
+        <div className="py-10 px-6 bg-white">
+          <input
+            type="text"
+            className="w-full px-4 py-3 border border-c-gray3 rounded-md text-f-dark focus:outline-c-primary"
+            placeholder="Enter branch name"
+          />
+        </div>
+        <footer className="border border-t-f-gray bg-white rounded-b-lg flex gap-2 justify-end py-6 px-4">
+          <button
+            className="w-28 h-12 text-f-dark text-p-rg font-semibold"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+          <button className="w-28 h-12 bg-[#3B7CF9] text-f-light text-p-rg font-semibold rounded-md hover:bg-[#77a0ec] active:bg-bg-[#2c4c86]">
+            Save
+          </button>
+        </footer>
+      </div>
+    </div>,
+    document.body // This renders the modal outside the usual DOM structure
+  );
+};
+
+export default AddBranchModal;
---
+++ frontend/src/Page/Main Page/OrgStaff.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import StaffCard from "../../Component/ui/StaffCard";
+import AddStaffModal from "../../Component/ui/AddStaffModal";
+import { useSelector } from "react-redux";
+import { IoIosAddCircleOutline } from "react-icons/io";
+
+const OrgStaff = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const staffs = useSelector((state) => state.reducer.staff.staffs);
+
+  const handleOpenStaffModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseStaffModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="w-full h-full flex flex-col items-center p-4 md:p-8">
+      <div className="w-full flex flex-row-reverse">
+        <div
+          className="h-12 w-40 flex justify-center items-center border-2 border-[#222222] rounded-md py-2 px-2 md:px-4 font-Poppins cursor-pointer"
+          onClick={handleOpenStaffModal}
+        >
+          <IoIosAddCircleOutline className="h-6 w-6 md:mr-2" />
+          <h1 className="hidden md:block">Add Staff</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 mt-4 items-center w-full h-full">
+        {staffs.length > 0 ? (
+          staffs.map((staff, index) => (
+            <StaffCard
+              key={index}
+              name={
+                `${staff.first_name || ""} ${staff.last_name || ""}`.trim() ||
+                staff.name
+              }
+              email={staff.email}
+              position={staff.position}
+            />
+          ))
+        ) : (
+          <p className="font-semibold text-3xl text-c-secondary">
+            No Active Staff found!
+          </p>
+        )}
+      </div>
+      {isModalOpen && <AddStaffModal onClose={handleCloseStaffModal} />}
+    </div>
+  );
+};
+
+export default OrgStaff;
