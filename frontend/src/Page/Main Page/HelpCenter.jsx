0c66f4217845b7fbef22faccd84feeffa31b8f0a
luisbandivas <jimboybandivas@gmail.com>
2024-10-22 22:24:09 +0800

    Added: sample help section content

--- frontend/src/Page/Main Page/HelpCenter.jsx
+++ frontend/src/Page/Main Page/HelpCenter.jsx
@@ -15,54 +15,49 @@ const HelpCenter = () => {
   };
 
   return (
-    <div className="text-f-dark p-3 md:p-6 xl:p-8 font-Poppin h-full flex flex-col md:flex-row gap-6">
-      <nav className="w-full md:w-1/3 flex flex-col justify-between h-fit md:h-full">
-        <div className="hidden md:block">
-          <h1 className="text-p-lg font-semibold">{selected}</h1>
-          <p className="text-p-rg">Lorem ipsum dolor sha.</p>
-        </div>
-        <div className="flex flex-col">
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-1 w-3/4">
-              {[
-                "Getting Started",
-                "Account",
-                "Billing",
-                "FAQ's",
-                "Features",
-                "Changelog",
-              ].map((section) => (
-                <div
-                  key={section}
-                  className={`h-auto flex items-center rounded-md px-4 py-2 ${
-                    selected === section
-                      ? "bg-[#E0EAEA] text-c-primary font-semibold"
-                      : "text-f-dark font-md"
-                  }`}
-                  onClick={() => handleSelected(section)}
-                >
-                  <IoIosAddCircleOutline className="h-6 w-6 md:mr-2" />
-                  <h1>{section}</h1>
-                </div>
-              ))}
-            </div>
-            <div className="md:block hidden w-full bg-c-primary rounded-lg px-5 py-6  text-f-light">
-              <section>
-                <h1 className="font-semibold text-p-lg">
-                  Do you still need our help?
-                </h1>
-                <p className="text-p-rg">Send you request via email.</p>
-              </section>
-              <div className="h-12 w-fit items-center justify-center rounded-md px-4 py-2 font-md bg-f-light text-f-dark font-semibold text-p-lg mt-6">
-                <h1>Contact Us</h1>
-              </div>
+    <div className="text-f-dark p-3 md:p-6 xl:p-8 font-Poppin h-full gap-6 overflow-clip">
+      <nav className="w-full flex gap-4 overflow-x-scroll">
+        {[
+          "Getting Started",
+          "Account",
+          "Billing",
+          "FAQ's",
+          "Features",
+          "Changelog",
+        ].map((section) => (
+          <div
+            key={section}
+            className={`h-auto flex items-center rounded-full px-4 py-2 cursor-pointer text-nowrap ${
+              selected === section
+                ? "bg-[#E0EAEA] text-c-primary border border-c-primary font-semibold"
+                : "text-f-dark font-medium border  bg-white"
+            }`}
+            onClick={() => handleSelected(section)}
+          >
+            <h1>{section}</h1>
+          </div>
+        ))}
+      </nav>
+      <div className="flex h-full pt-4 pb-12">
+        <section className="w-full md:w-1/3 flex flex-col justify-between h-fit md:h-full">
+          <div>
+            <h1 className="text-p-lg font-semibold">{selected}</h1>
+            <p className="text-p-rg">Lorem ipsum dolor sha.</p>
+          </div>
+          <div className="w-2/3 bg-c-primary rounded-lg px-4 py-4  text-f-light text-p-rg">
+            <section>
+              <h1 className="font-semibold">Do you still need our help?</h1>
+              <p className="text-p-sm">Send you request via email.</p>
+            </section>
+            <div className="h-fit w-fit items-center justify-center rounded-md px-4 py-2 font-md bg-f-light text-f-dark font-semibold mt-4">
+              <h1>Contact Us</h1>
             </div>
           </div>
-        </div>
-      </nav>
-      <aside className="w-full md:w-2/3 h-full overflow-scroll">
-        {selected && <HelpSections selected={selected} />}
-      </aside>
+        </section>
+        <aside className="w-full md:w-2/3 h-full overflow-y-scroll">
+          {selected && <HelpSections selected={selected} />}
+        </aside>
+      </div>
     </div>
   );
 };
--- frontend/src/Page/Main Page/ProfileSetting.jsx
+++ frontend/src/Page/Main Page/ProfileSetting.jsx
@@ -15,38 +15,38 @@ const ProfileSetting = () => {
   };
 
   return (
-    <div className="text-f-dark h-full p-4 md:p-6 xl:p-8 font-Poppins flex flex-col lg:flex-row gap-4 md:gap-8 ">
-      <nav className="lg:w-1/5 w-full flex flex-col gap-10">
-        <div>
-          <div className="flex flex-col gap-1 w-full">
-            <div
-              className={`h-auto flex items-center rounded-md px-4 py-2 cursor-pointer ${
-                selected === "My Profile"
-                  ? "bg-[#E0EAEA] text-c-primary font-semibold"
-                  : "text-f-dark font-md"
-              }`}
-              onClick={() => handleSelected("My Profile")}
-            >
-              <FiUser className="h-6 w-6 md:mr-2" />
-              <h1>My Profile</h1>
-            </div>
-            <div
-              className={`h-auto flex items-center rounded-md px-4 py-2 cursor-pointer ${
-                selected === "Account"
-                  ? "bg-[#E0EAEA] text-c-primary font-semibold"
-                  : "text-f-dark font-md"
-              }`}
-              onClick={() => handleSelected("Account")}
-            >
-              <FiUserCheck className="h-6 w-6 md:mr-2" />
-              <h1>Account</h1>
-            </div>
+    <div className="text-f-dark h-full p-4 md:p-6 xl:p-8 font-Poppins flex flex-col gap-4 md:gap-8 ">
+      <nav className="w-full">
+        <div className="flex gap-4 w-1/4">
+          <div
+            className={`h-auto flex items-center rounded-full px-4 py-2 cursor-pointer text-nowrap ${
+              selected === "My Profile"
+                ? "bg-[#E0EAEA] text-c-primary border border-c-primary font-semibold"
+                : "text-f-dark font-md border  bg-white"
+            }`}
+            onClick={() => handleSelected("My Profile")}
+          >
+            <FiUser className="h-6 w-6 md:mr-2" />
+            <h1>My Profile</h1>
+          </div>
+          <div
+            className={`h-auto flex items-center rounded-full px-4 py-2 cursor-pointer text-nowrap ${
+              selected === "Account"
+                ? "bg-[#E0EAEA] text-c-primary border border-c-primary font-semibold"
+                : "text-f-dark font-md border  bg-white"
+            }`}
+            onClick={() => handleSelected("Account")}
+          >
+            <FiUserCheck className="h-6 w-6 md:mr-2" />
+            <h1>Account</h1>
           </div>
         </div>
       </nav>
-      <aside className="lg:w-4/5 w-full overflow-y-scroll">
-        {selected && <ProfileSettingSection selected={selected} />}
-      </aside>
+      <div className="w-full flex justify-center overflow-y-scroll">
+        <div className="lg:w-3/5 w-full">
+          {selected && <ProfileSettingSection selected={selected} />}
+        </div>
+      </div>
     </div>
   );
 };
