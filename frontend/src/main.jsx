4f82c7640a0ec9a3955b9a31504519ed31fe9859
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-11-13 12:07:00 +0800

    Refactor: get clientId on env variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { AuthContextProvider } from "./Context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
 
-const clientId =
-  "425472986303-ik9s00kvanojru5i9hso7kue14mk39eh.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
