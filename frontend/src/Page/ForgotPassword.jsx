69df0cf6b99b1bbf19574114200dae360108a118
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-11-15 09:59:57 +0800

    Added: change password loading state

--- frontend/src/Page/ForgotPassword.jsx
+++ frontend/src/Page/ForgotPassword.jsx
@@ -15,6 +15,8 @@ const ForgotPassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [isPassVisible, setIsPassVisible] = useState(false);
   const [isCPassVisible, setIsCPassVisible] = useState(false);
   const [otp, setOtp] = useState(new Array(6).fill(""));
@@ -22,6 +24,7 @@ const ForgotPassword = () => {
   const navigate = useNavigate();
 
   const handleNext = async () => {
+    setIsLoading(true);
     try {
       if (currentCardIndex === 0) {
         if (email === "" || email === null) {
@@ -54,7 +57,10 @@ const ForgotPassword = () => {
           console.log("There is an error");
         }
       }
-    } catch (error) {}
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleBack = () => {
@@ -85,198 +91,200 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="h-screen w-full bg-bg-mc font-Raleway text-f-dark">
-      <div className="flex flex-col items-center justify-center h-full w-full">
-        {currentCardIndex < 3 ? (
-          <div className="flex gap-40 mb-20">
-            <div className="rounded-full bg-green-300 p-8"></div>
-            <div className="rounded-full bg-red-300 p-8"></div>
-            <div className="rounded-full bg-blue-300 p-8"></div>
-          </div>
-        ) : (
-          <div className="rounded-full bg-green-300 p-16 mb-10"></div>
-        )}
-        <div className="w-[600px] h-fit">
-          {currentCardIndex === 0 && (
-            <div className="flex flex-col gap-8">
-              <article className="flex flex-col text-center">
-                <h1 className="text-h-h4 font-semibold">Forgot Password?</h1>
-                <p className="text-p-sm md:text-p-rg text-f-gray2">
-                  No worries, well help you reset your password
-                </p>
-              </article>
-              <div className="flex flex-col gap-8 items-center">
-                <section className="w-full">
-                  <label className="text-p-sm md:text-p-rg font-medium">
-                    Email Address
-                  </label>
-                  <div className="relative">
-                    <MdOutlineEmail className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
-                    <input
-                      type="email"
-                      name="email"
-                      className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
-                      placeholder="Enter your email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                </section>
-                <button
-                  className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
-                  onClick={handleNext}
-                >
-                  Send an OTP
-                </button>
-              </div>
+    <>
+      <div className="h-screen w-full bg-bg-mc font-Raleway text-f-dark">
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          {currentCardIndex < 3 ? (
+            <div className="flex gap-40 mb-20">
+              <div className="rounded-full bg-green-300 p-8"></div>
+              <div className="rounded-full bg-red-300 p-8"></div>
+              <div className="rounded-full bg-blue-300 p-8"></div>
             </div>
+          ) : (
+            <div className="rounded-full bg-green-300 p-16 mb-10"></div>
           )}
-          {currentCardIndex === 1 && (
-            <div className="flex flex-col gap-8">
-              <article className="flex flex-col text-center">
-                <h1 className="text-h-h4 font-semibold">Confirm Your Code</h1>
-                <p className="text-p-sm md:text-p-rg text-f-gray2">
-                  Please enter the 6-digit code we sent to your email.
-                </p>
-              </article>
-              <div className="flex flex-col gap-8 items-center">
-                <div className="flex w-full gap-4">
-                  {otp.map((_, index) => (
-                    <input
-                      key={index}
-                      type="text"
-                      className="w-full h-20 text-center border border-f-gray rounded-md text-lg text-f-dark focus:outline-c-primary"
-                      maxLength="1"
-                      value={otp[index]}
-                      onChange={(e) => handleChange(e.target, index)}
-                      onKeyDown={(e) => handleKeyDown(e, index)}
-                      ref={(el) => (inputsRef.current[index] = el)}
-                      autoFocus={index === 0}
-                    />
-                  ))}
+          <div className="w-[600px] h-fit">
+            {currentCardIndex === 0 && (
+              <div className="flex flex-col gap-8">
+                <article className="flex flex-col text-center">
+                  <h1 className="text-h-h4 font-semibold">Forgot Password?</h1>
+                  <p className="text-p-sm md:text-p-rg text-f-gray2">
+                    No worries, well help you reset your password
+                  </p>
+                </article>
+                <div className="flex flex-col gap-8 items-center">
+                  <section className="w-full">
+                    <label className="text-p-sm md:text-p-rg font-medium">
+                      Email Address
+                    </label>
+                    <div className="relative">
+                      <MdOutlineEmail className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
+                      <input
+                        type="email"
+                        name="email"
+                        className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                  </section>
+                  <button
+                    className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
+                    onClick={handleNext}
+                  >
+                    Send an OTP
+                  </button>
                 </div>
+              </div>
+            )}
+            {currentCardIndex === 1 && (
+              <div className="flex flex-col gap-8">
+                <article className="flex flex-col text-center">
+                  <h1 className="text-h-h4 font-semibold">Confirm Your Code</h1>
+                  <p className="text-p-sm md:text-p-rg text-f-gray2">
+                    Please enter the 6-digit code we sent to your email.
+                  </p>
+                </article>
+                <div className="flex flex-col gap-8 items-center">
+                  <div className="flex w-full gap-4">
+                    {otp.map((_, index) => (
+                      <input
+                        key={index}
+                        type="text"
+                        className="w-full h-20 text-center border border-f-gray rounded-md text-lg text-f-dark focus:outline-c-primary"
+                        maxLength="1"
+                        value={otp[index]}
+                        onChange={(e) => handleChange(e.target, index)}
+                        onKeyDown={(e) => handleKeyDown(e, index)}
+                        ref={(el) => (inputsRef.current[index] = el)}
+                        autoFocus={index === 0}
+                      />
+                    ))}
+                  </div>
+                  <button
+                    className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
+                    onClick={handleNext}
+                  >
+                    Confirm OTP
+                  </button>
+                  <p className="text-p-sc md:text-p-sm font-medium text-center w-full">
+                    Didn't receive the email?{" "}
+                    <span className="text-c-secondary font-bold">
+                      Click to resend
+                    </span>
+                  </p>
+                </div>
+              </div>
+            )}
+            {currentCardIndex === 2 && (
+              <div className="flex flex-col gap-8">
+                <article className="flex flex-col text-center">
+                  <h1 className="text-h-h4 font-semibold">
+                    Create a New Password
+                  </h1>
+                  <p className="text-p-sm md:text-p-rg text-f-gray2">
+                    Create a new password to secure your account.
+                  </p>
+                </article>
+                <div className="flex flex-col gap-8 items-center">
+                  <section className="w-full">
+                    <label className="text-p-sm md:text-p-rg font-medium">
+                      New Password
+                    </label>
+                    <div className="relative">
+                      <MdOutlineLock className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
+                      <input
+                        type={isPassVisible ? "text" : "password"}
+                        name="password"
+                        className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
+                        placeholder="Enter your new password"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                      />
+                      <button
+                        type="button"
+                        className="absolute top-6 right-4 text-f-gray2"
+                        onClick={() => setIsPassVisible(!isPassVisible)}
+                      >
+                        {isPassVisible ? (
+                          <MdOutlineRemoveRedEye className="w-6 h-6" />
+                        ) : (
+                          <FaRegEyeSlash className="w-6 h-6" />
+                        )}
+                      </button>
+                    </div>
+                  </section>
+                  <section className="w-full">
+                    <label className="text-p-sm md:text-p-rg font-medium">
+                      Confirm Password
+                    </label>
+                    <div className="relative">
+                      <MdOutlineLock className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
+                      <input
+                        type={isCPassVisible ? "text" : "password"}
+                        name="confirmpassword"
+                        className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
+                        placeholder="Confirm your password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                      />
+                      <button
+                        type="button"
+                        className="absolute top-6 right-4 text-f-gray2"
+                        onClick={() => setIsCPassVisible(!isCPassVisible)}
+                      >
+                        {isCPassVisible ? (
+                          <MdOutlineRemoveRedEye className="w-6 h-6" />
+                        ) : (
+                          <FaRegEyeSlash className="w-6 h-6" />
+                        )}
+                      </button>
+                    </div>
+                  </section>
+                  <button
+                    className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
+                    onClick={handleNext}
+                  >
+                    Reset Password
+                  </button>
+                </div>
+              </div>
+            )}
+            {currentCardIndex === 3 && (
+              <div className="flex flex-col gap-8">
+                <article className="flex flex-col text-center">
+                  <h1 className="text-h-h4 font-semibold">
+                    Password Reset Successful!
+                  </h1>
+                  <p className="text-p-sm md:text-p-rg text-f-gray2">
+                    Success! Your password has been updated. You can now log in
+                    using your new password.
+                  </p>
+                </article>
                 <button
                   className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
-                  onClick={handleNext}
+                  onClick={() => navigate("/")}
                 >
-                  Confirm OTP
-                </button>
-                <p className="text-p-sc md:text-p-sm font-medium text-center w-full">
-                  Didn't receive the email?{" "}
-                  <span className="text-c-secondary font-bold">
-                    Click to resend
-                  </span>
-                </p>
-              </div>
-            </div>
-          )}
-          {currentCardIndex === 2 && (
-            <div className="flex flex-col gap-8">
-              <article className="flex flex-col text-center">
-                <h1 className="text-h-h4 font-semibold">
-                  Create a New Password
-                </h1>
-                <p className="text-p-sm md:text-p-rg text-f-gray2">
-                  Create a new password to secure your account.
-                </p>
-              </article>
-              <div className="flex flex-col gap-8 items-center">
-                <section className="w-full">
-                  <label className="text-p-sm md:text-p-rg font-medium">
-                    New Password
-                  </label>
-                  <div className="relative">
-                    <MdOutlineLock className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
-                    <input
-                      type={isPassVisible ? "text" : "password"}
-                      name="password"
-                      className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
-                      placeholder="Enter your new password"
-                      value={newPassword}
-                      onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                    <button
-                      type="button"
-                      className="absolute top-6 right-4 text-f-gray2"
-                      onClick={() => setIsPassVisible(!isPassVisible)}
-                    >
-                      {isPassVisible ? (
-                        <MdOutlineRemoveRedEye className="w-6 h-6" />
-                      ) : (
-                        <FaRegEyeSlash className="w-6 h-6" />
-                      )}
-                    </button>
-                  </div>
-                </section>
-                <section className="w-full">
-                  <label className="text-p-sm md:text-p-rg font-medium">
-                    Confirm Password
-                  </label>
-                  <div className="relative">
-                    <MdOutlineLock className="w-6 h-6 absolute top-5 left-4 text-c-gray3" />
-                    <input
-                      type={isCPassVisible ? "text" : "password"}
-                      name="confirmpassword"
-                      className="mt-1 w-full pl-12 py-4 border border-c-gray3 font-medium rounded-md text-f-dark focus:outline-c-primary font-Poppins"
-                      placeholder="Confirm your password"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                    <button
-                      type="button"
-                      className="absolute top-6 right-4 text-f-gray2"
-                      onClick={() => setIsCPassVisible(!isCPassVisible)}
-                    >
-                      {isCPassVisible ? (
-                        <MdOutlineRemoveRedEye className="w-6 h-6" />
-                      ) : (
-                        <FaRegEyeSlash className="w-6 h-6" />
-                      )}
-                    </button>
-                  </div>
-                </section>
-                <button
-                  className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
-                  onClick={handleNext}
-                >
-                  Reset Password
+                  Go to Login
                 </button>
               </div>
-            </div>
-          )}
-          {currentCardIndex === 3 && (
-            <div className="flex flex-col gap-8">
-              <article className="flex flex-col text-center">
-                <h1 className="text-h-h4 font-semibold">
-                  Password Reset Successful!
-                </h1>
-                <p className="text-p-sm md:text-p-rg text-f-gray2">
-                  Success! Your password has been updated. You can now log in
-                  using your new password.
-                </p>
-              </article>
-              <button
-                className="text-center py-3 rounded-md text-p-rg md:text-p-lg text-f-light font-semibold bg-c-primary w-full"
+            )}
+            {currentCardIndex < 3 && (
+              <div
+                className="flex items-center justify-center text-f-gray2 w-full mt-8 gap-2"
                 onClick={() => navigate("/")}
               >
-                Go to Login
-              </button>
-            </div>
-          )}
-          {currentCardIndex < 3 && (
-            <div
-              className="flex items-center justify-center text-f-gray2 w-full mt-8 gap-2"
-              onClick={() => navigate("/")}
-            >
-              <FaArrowLeft className="h-auto w-6" />
-              <p className="text-p-sc md:text-p-sm font-medium">
-                Back to login
-              </p>
-            </div>
-          )}
+                <FaArrowLeft className="h-auto w-6" />
+                <p className="text-p-sc md:text-p-sm font-medium">
+                  Back to login
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
